e1c5e79f18f6e75f5d414a6ff18a20b4b25d668c
inho9377 <inho9377@naver.com>
2019-10-06 21:16:28 +0900

    정리

---
+++ Unity/Assets/Script/GameController/BattleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BattleController : MonoBehaviour
+{
+    public Text CommandLog;
+
+    public Camera UICamera;
+
+
+}
---
+++ Unity/Assets/Script/GameController/InputController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+	public void OnChang(string command)
+	{
+		
+	}
+	
+	public void OnInput(string command)
+	{
+		
+	}
+}
