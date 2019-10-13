f2f2acb5169d533edf38e5803b7650b6dabdba14
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2019-10-13 23:14:23 +0900

    Resource 시스템 추가

---
+++ Unity/Assets/Script/Resource/ResourceBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBase {
+
+    public readonly int id;
+    public readonly string name;
+
+    //public Sprite sprite;
+
+    public ResourceBase(Dictionary<string, object> jsonRaw) {
+        //
+        if (jsonRaw.ContainsKey("id")) {
+            id = int.Parse(jsonRaw["id"].ToString());
+        }
+
+        //
+        if (jsonRaw.ContainsKey("name")) {
+            name = jsonRaw["name"].ToString();
+        }
+
+        //if (jsonRaw.ContainsKey("sprite")) {
+        //    var _path = jsonRaw["sprite"].ToString();
+        //    imgStage = Utility.LoadResource<Sprite>(_path);
+        //}
+    }
+}
---
+++ Unity/Assets/Script/Resource/ResourceManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ResourceManager {
+
+    private static ResourceManager singleton = new ResourceManager();
+    private bool inited = false;
+    const string PATCH_FOLDER_NAME = "Patches";
+    const string DIR_PATCHES = "Assets/" + PATCH_FOLDER_NAME + "/";
+
+    //
+    //private Dictionary<long, ResourceStage> stages = new Dictionary<long, ResourceStage>();
+
+    ////
+    //public ICollection<ResourceStage> GetStages() {
+    //    return stages.Values;
+    //}
+
+    public static ResourceManager Get() {
+        if (singleton == null)
+            singleton = new ResourceManager();
+
+        return singleton;
+    }
+
+    public void Initialize() {
+        if (singleton.inited)
+            return;
+
+        //
+        ReloadAll();
+        //InitStages();
+
+        //
+        singleton.inited = true;
+    }
+
+    private void ReloadAll() {
+        //
+        string _folderName = "Json";
+
+        //// TODO: 컨테이너 정리 필요
+        //stages.Clear();
+
+        ////
+        //foreach (string textAssetName in Utility.GetFileNamesAtPath("Patches/" + _folderName)) {
+        //    var ta = Utility.LoadPatches<TextAsset>(_folderName + "/" + textAssetName);
+        //    var parsed = Utility.ParseJSONfromTextAsset(ta);
+
+        //    foreach (var jStage in parsed) {
+        //        //
+        //        var resStage = new ResourceStage(jStage);
+        //        stages.Add(resStage.id, resStage);
+        //    }
+
+        //    //
+        //    Resources.UnloadAsset(ta);
+        //    Debug.Log(textAssetName + " : " + parsed.Count);
+        //}
+    }
+}
