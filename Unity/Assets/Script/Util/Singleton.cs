f806ffd8afe20498b4adceb32ef3ff3c80990dc7
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2019-10-13 23:30:28 +0900

    MonoSingleton은 그 자체로 MonoBehaviour이기 때문에, T의 조건을 MonoBehaviour로 한정지을 필요가 없다 불필요한 Singleton 제거, 로직 일부 수정

--- Unity/Assets/Script/Util/Singleton.cs
+++ Unity/Assets/Script/Util/Singleton.cs
@@ -2,35 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    protected static T instance = null;
-    public static T Instance
-    {
-        get
-        {
-            instance = FindObjectOfType(typeof(T)) as T;
-            if (instance == null)
-            {
-                instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
-                DontDestroyOnLoad(instance);
-            }
+public class MonoSingleton<T> : MonoBehaviour where T : Object {
+    private static T instance;
+
+    public static T Instance {
+        get {
+            if (instance != null)
+                return instance;
+
+            // null이면 scene에서 찾아보고
+            instance = FindObjectOfType<T>();
+            if (instance != null)
+                return instance;
+
+            // 그럼에도 없으면 새로 생성한다.
+            instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+            DontDestroyOnLoad(instance);
+
             return instance;
         }
-    }
-}
-
-public class Singleton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
