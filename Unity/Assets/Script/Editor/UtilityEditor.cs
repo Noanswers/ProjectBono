7adb4bdc59fa9212b2d74abeee86799c67bfddfc
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2019-10-15 05:10:24 +0900

    Ctrl H 단축키로 시작할 수 있도록 수정, 무조건 intro scene 부터 시작함

---
+++ Unity/Assets/Script/Editor/UtilityEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class UtilityEditor : EditorWindow {
+    [MenuItem("Play/PlayStartScene %h")]
+    public static void PlayStartScene() {
+        if (EditorSceneManager.GetActiveScene().isDirty) {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        EditorSceneManager.OpenScene("Assets/Scenes/IntroScene.unity");
+        EditorApplication.isPlaying = true;
+    }
+}
