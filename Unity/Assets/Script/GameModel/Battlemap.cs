c46e36830c728e364177754a1c2ffeeb9792d355
inho9377 <inho9377@naver.com>
2019-10-27 23:08:48 +0900

    [Test] TileMap Collider SetColor

---
+++ Unity/Assets/Script/GameModel/Battlemap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Battlemap : MonoSingleton<Battlemap>
+{
+    public Tilemap _TileMap;
+
+    public void TileUp(Vector2 worldPos) {
+
+        int x = _TileMap.WorldToCell(worldPos).x;
+        int y = _TileMap.WorldToCell(worldPos).y;
+
+        Vector3Int tileVector = new Vector3Int(x, y, 0);
+        _TileMap.SetTileFlags(tileVector, TileFlags.None);
+        _TileMap.SetColor(tileVector, Color.red);
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity/Assets/Script/GameModel/PlayerUnit.cs
+++ Unity/Assets/Script/GameModel/PlayerUnit.cs
@@ -9,6 +9,7 @@ public class PlayerUnit : BaseUnit {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Move(h, v);
+        Battlemap.Instance.TileUp(transform.position);
     }
 
     public void Move(float h, float v) {
